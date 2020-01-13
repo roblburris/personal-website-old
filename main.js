802d347b9020b4086ac6ad6a21e90feeb784158a
Robert Burris <roblburris@gmail.com>
2020-01-13 09:01:18 -0800

    updated website sections, resume, added new project

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-console.log('helllooooo');
+console.log('website is up and running!');
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -18,13 +18,13 @@ app.get('/projects', function(req, res) {
   res.render('projects');
 })
 
-app.get('/personal', function(req, res) {
-  res.render('personal');
-})
+// app.get('/personal', function(req, res) { will add personal section later
+  // res.render('personal');
+// })
 
 
 app.get('/contact', function(req, res) {
-  res.render('index');
+  res.render('contact');
 })
 
 app.listen(3000);
